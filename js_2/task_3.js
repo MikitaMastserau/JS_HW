b007e558d17ae0b38719a99ce29f3e594627b898
MikitaMastserau <nik.redko@gmail.com>
2022-12-08 18:36:58 +0300

    hw 2

--- js_2/task_3.js
+++ js_2/task_3.js
@@ -1,3 +1,7 @@
+/*Функция принимает массив чисел. Возвращает массив строк такого вида:
+['Value: 3; Index: 0; Squared: 9','Value: 4; Index: 1; Squared: 16','Value: 6; Index: 2; Squared: 36']
+*/
+
 const infoNumbers = (numbers) => {
     if (!Array.isArray(numbers)) return;
 
